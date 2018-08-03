695581a2ec818894832f4aba35800f1b362a5322
Ira Miller <public@iramiller.com>
2018-08-03 07:09:48 -0500

    cached

--- cli.js
+++ cli.js
@@ -1,100 +1,55 @@
 #!/usr/bin/env node
+const VERSION = require('./package.json').version
 const program = require('commander')
 const path = require('path')
-const {
-  // init,
-  status,
-  log,
-  add,
-  remove,
-  commit,
-  clone,
-  fetch,
-  push,
-  pull
-} = require('guld-git')
-const VERSION = require('./package.json').version
+const { commodity } = require('ledger-types')
+const home = require('user-home')
+const CoinMarketCap = require('coinmarketcap-api')
+const client = new CoinMarketCap()
+const { getFS } = require('guld-fs')
+var fs
 
 /* eslint-disable no-console */
 program
-  .name('guld-git')
+  .name('guld-market')
   .version(VERSION)
-  .description('Guld standardized CLI for git.')
-  .option('-C --cwd <dir>', 'Set current working directory before running.', d => {
-    process.chdir(path.resolve(d))
-  })
-  .command('path', 'Blocktree path resolution tools.')
-  .command('config', 'Configuration manager for git config files.')
-  .command('host', 'Configuration manager for git hosts.')
-  .command('remote', 'Manage git remotes the guld way.')
-  .command('add [file]')
-  .description('Add files to the git working directory.')
-  .option('-A --all', 'add changes from all tracked and untracked files')
-  .action((file, options) => {
-    if (file && !options.all) add(file, process.cwd())
-    else add(file, process.cwd()).then(console.log('Ok.'))
+  .description('Guld market information and tools.')
+  .command('price [asset]')
+  .description('Get the price of the given asset (Default: GULD)')
+  .action(async (asset, options) => {
+    asset = asset || 'GULD'
+    var price = await commodity.getCommodityPrice(asset, 'USD', 'isysd')
+    console.log(commodity.parseCommodityPrice(price, asset).value.toString())
   })
 program
-  .command('init')
-  .description('Create an empty Git repository or reinitialize an existing one.')
-  .action(async (options) => {
-    await remove(process.cwd())
-  })
-program
-  .command('clone [url] [directory]')
-  .description('Clone a repository into a new directory.')
-  .action(async (url, directory, options) => {
-    await clone(process.cwd(), directory, url)
-  })
-program
-  .command('remove <file>')
-  .alias('delete')
-  .description('Remove files from the git working directory.')
-  .option('--cached', 'Keep the cached (working) copy of the file.')
-  .action(async (file, options) => {
-    await remove(file, process.cwd(), options.cached)
-  })
-program
-  .command('log [fromRef]')
-  .description('List commits from the given ref backwards.')
-  .option('-d --depth <depth>', 'The depth to search to.', 1)
-  .action(async (fromRef, options) => {
-    console.log((await log(process.cwd(), fromRef, options.depth)).join('\n'))
-  })
-program
-  .command('status [file]')
-  .description('Get the status of a file in the working directory.')
-  .action(async (file, options) => {
-    var stat = await status(file, process.cwd())
-    if (stat) console.log(stat.join('\n'))
-  })
-program
-  .command('commit [message]')
-  .description('Stores the current contents of the index in a new commit along with a log message from the user describing the changes.')
-  .action(async (message, options) => {
-    await commit(process.cwd(), message)
-    console.log('Ok.')
-  })
-program
-  .command('fetch [remote] [branch]')
-  .description('Download objects and refs from another repository.')
-  .action(async (remote, branch, options) => {
-    await fetch(process.cwd(), remote, branch)
-    console.log('Ok.')
-  })
-program
-  .command('push [remote] [branch]')
-  .description('Update remote refs along with associated objects.')
-  .action(async (remote, branch, options) => {
-    await push(process.cwd(), remote, branch)
-    console.log('Ok.')
-  })
-program
-  .command('pull [remote] [branch]')
-  .description('Fetch from and integrate with another repository or a local branch.')
-  .action(async (remote, branch, options) => {
-    await pull(process.cwd(), remote, branch)
-    console.log('Ok.')
+  .command('update [asset]')
+  .description('Update the price of the given asset (Default: all)')
+  .action(async (ass, options) => {
+    var exchange = options.exchange || 'coinmarketcap'
+    fs = fs || await getFS()
+    async function updateAsset (asset) {
+      console.log(asset)
+      var ticker = await client.getTicker({currency: asset})
+      var roundprice = Math.round(ticker.data.quotes.USD.price * 100000) / 100000
+      var invprice = Math.round((1 / ticker.data.quotes.USD.price) * 100000) / 100000
+      var time = Math.trunc(Date.now() / 1000)
+      var date = new Date(time * 1000)
+      var ta = date.toISOString().split('T')
+      var datestr = `${ta[0].replace(/-/g, '/')}`
+      var timestr = `${ta[1].split('.')[0]}`
+      var pdb = `P ${datestr} ${timestr} ${asset.toUpperCase()} $${roundprice}
+P ${datestr} ${timestr} $ ${invprice} ${asset.toUpperCase()}
+P ${datestr} ${timestr} ${asset.toUpperCase()} ${roundprice} USD
+P ${datestr} ${timestr} USD ${invprice} ${asset.toUpperCase()}
+`
+      await fs.mkdirp(path.join(home, 'market', 'USD', asset, 'prices'))
+      await fs.writeFile(path.join(home, 'market', 'USD', asset, 'prices', `${exchange}.dat`), pdb)
+    }
+    if (ass && ass !== undefined) updateAsset(ass)
+    else {
+      var list = (await fs.readdir(path.join(home, 'market', 'USD'))).filter(e => !e.startsWith('.') && e !== 'GULD') // remove once GULD lists on coinmarketcap
+      await Promise.all(list.map(e => updateAsset(e).catch(e => {}))).catch(e => {})
+    }
   })
 /* eslint-enable no-console */
 
